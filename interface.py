fbe4a0e9c75c7372eb226082e8fdd96098601c68
reblapointe <reblapointe@gmail.com>
2026-08-31 09:53:34 -0400

    Démo pandas

---
+++ interface.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/broches')
+def broches():
+    broches = [2, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.5, 5, 5.5, 6, 6.5, 7, 8, 9, 10]
+    return render_template('broches.html', broches=broches)
+
+
+@app.route('/echantillon')
+def donnees():
+    # Chargement du DataFrame
+    df = modele.get_donnees()
+    
+    # Conversion en listes pour Jinja2
+    columns = df.columns.tolist()
+    rows = df.values.tolist()
+    print(columns)
+    return render_template("echantillon.html", columns=columns, rows=rows)
+
+@app.route('/demo')
+def demo():
+    nom = request.args.get('nom', 'le monde')
+    return f"Bonjour {nom}!"
---
+++ models/modele.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+
+def get_donnees():
+    # Chargement du DataFrame
+    current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    csv_path = os.path.join(current_dir, 'data', 'echantillon.csv')
+    df = pd.read_csv(csv_path, sep=',')
+    
+    return df
